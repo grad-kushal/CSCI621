c132cdcb750623ed35874ad0dd323963447a5d97
grad-kushal <ksk7657@g.rit.edu>
2022-06-25 20:27:55 -0400

    Refactor the main class

--- EmployeeInformation/app/src/main/java/edu/rit/ibd/a4/EmployeeInformationApplication.java
+++ EmployeeInformation/app/src/main/java/edu/rit/ibd/a4/EmployeeInformationApplication.java
@@ -1,7 +1,6 @@
 package edu.rit.ibd.a4;
 
 import java.util.*;
-import java.util.Date;
 
 import com.github.javafaker.Faker;
 import com.mongodb.client.MongoCollection;
@@ -104,68 +103,4 @@ public class EmployeeInformationApplication {
 		return client;
 	}
 
-}
-
-class Employee {
-
-	static int empId = 0;
-
-	int id;
-	String fname;
-	String lname;
-	String photo;
-	Date dob;
-	String cell;
-	String email;
-	String address;
-	String role;
-	String department;
-	int manager;
-	ArrayList<Long> manages;
-	String emergency_contact;
-	Date joining;
-	Date termination;
-
-	@Override
-	public String toString() {
-		return "Employee{" +
-				"id=" + id +
-				", fname='" + fname + '\'' +
-				", lname='" + lname + '\'' +
-				", photo='" + photo + '\'' +
-				", dob=" + dob +
-				", cell='" + cell + '\'' +
-				", email='" + email + '\'' +
-				", address='" + address + '\'' +
-				", role='" + role + '\'' +
-				", department='" + department + '\'' +
-				", manager=" + manager +
-				", manages=" + manages +
-				", emergency_contact='" + emergency_contact + '\'' +
-				", joining=" + joining +
-				", termination=" + termination +
-				'}';
-	}
-
-	public Employee(Faker faker) {
-		this.id = empId++;
-		this.fname = faker.name().firstName();
-		this.lname = faker.name().lastName();
-		this.address = faker.address().fullAddress();
-		this.cell = faker.phoneNumber().cellPhone();
-		this.department = (this.id == 0) ? "Executive" : EmployeeInformationApplication.departments.get((this.id% EmployeeInformationApplication.departments.size()));
-		this.dob = faker.date().birthday();
-		this.email = fname.toLowerCase() + "." + lname.toLowerCase() + "@dbsimail.com";
-		this.emergency_contact = faker.phoneNumber().cellPhone();
-		this.joining = faker.date().between(dob, new Date());
-		this.manager = (this.id == 0) ? 9999 : -1;
-		this.manages = new ArrayList<>();
-		this.photo = null;
-		this.role = (this.id == 0) ? "CEO" : getRole(department);
-		this.termination = faker.date().between(joining, new Date());
-	}
-
-	private String getRole(String department) {
-		return department + " Role";
-	}
 }
